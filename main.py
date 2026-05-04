470906c22e76b1f18a94a0631ba6f3adcbb916f1
m_omar <umer.mushtaq@u-paris2.fr>
2026-05-04 22:29:53 +0200

    odd/even function

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ cwd = os.getcwd()
 current_dir = cwd.split("\\")[-1]
 
 print("Current directory: " + current_dir)
+
+
+def check_even_odd(num):
+    """Check if a number is even or odd."""
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+
+# Examples
+print("\nChecking if numbers are even or odd:")
+numbers = [1, 2, 3, 4, 5, 10, 15, 20, 42, 99]
+for number in numbers:
+    result = check_even_odd(number)
+    print(f"{number} is {result}")
