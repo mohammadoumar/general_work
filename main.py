de8599bbf5b0656da3fdc8cd29af5870443e6f9b
m_omar <umer.mushtaq@u-paris2.fr>
2026-05-05 19:02:06 +0200

    added prime fux

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ def check_even_odd(num):
         return "even"
     else:
         return "odd"
+    
+def is_prime(num):
+    """Check if a number is prime."""
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 
 # Examples
@@ -20,3 +29,13 @@ numbers = [1, 2, 3, 4, 5, 10, 15, 20, 42, 99]
 for number in numbers:
     result = check_even_odd(number)
     print(f"{number} is {result}")
+
+print("\nChecking if numbers are prime:")
+for number in numbers:
+    if is_prime(number):
+        print(f"{number} is prime")
+    else:
+        print(f"{number} is not prime")
+# for number in numbers:
+#     result = check_even_odd(number)
+#     print(f"{number} is {result}")
